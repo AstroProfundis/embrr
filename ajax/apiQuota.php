fdb59ac238f29c9b35f3cf7da7b6bda186a042e0
AstroProfundis <github@mail.atr.me>
2013-03-02 19:50:06 +0800

    Added: mentions API rate limit

--- ajax/apiQuota.php
+++ ajax/apiQuota.php
@@ -5,11 +5,17 @@
 	}
 	$t = getTwitter();
 	$limit = get_object_vars($t->ratelimit()->resources->statuses);
-	$limit = $limit["/statuses/home_timeline"];
-	$reset = intval($limit->reset - $_SERVER['REQUEST_TIME']);
-	$remaining = $limit->remaining < 0 ? 0 : $limit->remaining;
-	$qlimit = $limit->limit;
+	$timeline_limit = $limit["/statuses/home_timeline"];
+	$mentions_limit = $limit['/statuses/mentions_timeline'];
+	$timeline_reset = intval($timeline_limit->reset - $_SERVER['REQUEST_TIME']);
+	$mentions_reset = intval($mentions_limit->reset - $_SERVER['REQUEST_TIME']);
+	$timeline_remaining = $timeline_limit->remaining < 0 ? 0 : $timeline_limit->remaining;
+	$mentions_remaining = $mentions_limit->remaining < 0 ? 0 : $mentions_limit->remaining;
+	$timeline_qlimit = $timeline_limit->limit;
+	$mentions_qlimit = $mentions_limit->limit;
 	header('Content-Type: text/html');
-	echo "<li><span style=\"color: #2276BB\">Timeline API remains: $remaining/$qlimit</span></li>
-	<li><span style=\"color: #2276BB\">Reset in $reset sec(s)</span></li>";
+	echo "<li><span style=\"color: #2276BB\">Timeline API remains: $timeline_remaining/$timeline_qlimit</span></li>
+	<li><span style=\"color: #2276BB\">Reset in $timeline_reset sec(s)</span></li>
+	<li><span style=\"color: #2276BB\">Mentions API remains: $mentions_remaining/$mentions_qlimit</span></li>
+	<li><span style=\"color: #2276BB\">Reset in $mentions_reset sec(s)</span></li>";
 ?>
