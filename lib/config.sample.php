580c5bff24101128e8c08028b6ab69fcd742a4c6
Allen Zhong <moeallenz@gmail.com>
2014-02-02 22:45:43 +0800

    Update config sample Add documentation, as discussed in issue #50

--- lib/config.sample.php
+++ lib/config.sample.php
@@ -1,16 +1,39 @@
 <?php
+	/* API Setting
+	 *
+	 * This is the URL embrr used for all API calls, DO NOT change unless you know what you're doing!    *
+	 */
 	define('API_URL', 'https://api.twitter.com/1.1');
 	
-	// Config
-	define('SECURE_KEY', 'YOU SHOULD CHANGE IT'); //You should NOT leave it blank
-	define('BASE_URL','http://embr.in'); //Where your embr is hosted, i.e. http://bugbug.tk
+	/* Basic Configuration
+	 * 
+	 * - A SECURE_KEY is a string that being used to encrypt and decrypt cookies, you should NOT leave   *
+	 * it empty due to security reasons, a long random string is usually an option.                      *
+	 *
+	 * - The BASE_URL is where your embrr hosted and will be used in many in-site links.                 *
+	 *
+	 * - The CONSUMER_KEY and CONSUMER_SECRET is two string that Twitter used to authorize applications, *
+	 * you can find them on the app detail page at https://api.twitter.com. You can leave the default    *
+	 * values unchanged if you don't know how to create a new app on twitter or you can't access twitter *
+	 * website, but this may be unsafe for you and your account. Use at your own risk!                   *
+	 */
+	define('SECURE_KEY', 'YOU SHOULD CHANGE IT');
+	define('BASE_URL','http://embr.in');
 	define("CONSUMER_KEY", "TEItTaPASySnYxziOyIdag");
 	define("CONSUMER_SECRET", "xJEoWvBumpqgiiBuviWTa7GT8KCvP7Kv3n0hixhJaZY");
 
-	define('ID_AUTH',false); // if you set id_auth true, u MUST set up the following AUTH_ID list
+	/* Private Setting
+	 *
+	 * If you set ID_AUTH true, only whitelisted user can login and use this embrr, you must set up the *
+	 * following AUTH_ID if you do so. Authorized user ID is seperated by ','.                          *
+	 */
+	define('ID_AUTH',false);
 	$AUTH_ID = array('username1','username2','username3','......');
 
-	//Optional Information
-	define('SITE_OWNER', 'TWITTER'); //Your Twitter ID  
-	define('BLOG_SITE',''); //blog_site
+	/* Optional Information
+	 *
+	 * Here you can set the site owner's Twitter ID and website URL, they will show at the footer.      *
+	 */
+	define('SITE_OWNER', 'TWITTER');
+	define('BLOG_SITE','');
 ?>
