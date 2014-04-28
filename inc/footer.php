7b337c9a6d0771016f82bce63e27069e6e22ff2f
Allen Zhong <github@mail.atr.me>
2014-04-28 13:33:23 +0800

    Update footer.php
    It's an HTML...

--- inc/footer.php
+++ inc/footer.php
@@ -4,7 +4,7 @@
 		<div class="clear"></div>
 			<footer class="round">
 			<ul>
-			<li>&copy; 2010-date("Y") Contributors incl. <a href="profile.php" title="It is you that make it!" target="_blank"><?php echo getEncryptCookie('twitese_name')?></a></li>
+			<li>&copy; 2010-<?php echo date("Y") ?> Contributors incl. <a href="profile.php" title="It is you that make it!" target="_blank"><?php echo getEncryptCookie('twitese_name')?></a></li>
 			<?php if (BLOG_SITE) { ?><li><a href="<?php echo BLOG_SITE ?>" title="Site Owner's Blog" target="_blank">Blog</a></li><?php }?>
 			<li><a href="http://code.google.com/p/tuite/" target="_blank" title="Embr is proundly powered by the Open Source project - Twitese & Rabr">Twitese</a></li>
 			<li><a href="https://github.com/AstroProfundis/embrr" target="_blank">Open Source</a></li>
