ea0dfc5a56f84a86df618db950c072de72835ae6
shadowglen <shadowglen@me.com>
2014-05-24 10:32:40 +0000

    Remove the rarely used "share" function

--- inc/header.php
+++ inc/header.php
@@ -53,7 +53,6 @@ body {font-size:<?php echo $fontsize ?> !important;<?php
 				<?php $scheme=(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != "on") ? 'http://' : 'https://';
 				$base_url=str_replace('http://',$scheme,BASE_URL);
 				?> 
-				<li><a class="share" title="Drag me to share!" href="javascript:var%20d=document,w=window,f='<?php echo $base_url."/share.php" ?>',l=d.location,e=encodeURIComponent,p='?u='+e(l.href)+'&t='+e(d.title)+'&d='+e(w.getSelection?w.getSelection().toString():d.getSelection?d.getSelection():d.selection.createRange().text)+'&s=bm';a=function(){if(!w.open(f+p,'sharer','toolbar=0,status=0,resizable=0,width=600,height=300,left=175,top=150'))l.href=f+'.new'+p};if(/Firefox/.test(navigator.userAgent))setTimeout(a,0);else{a()}void(0);">Share</a></li>
 				<li><a href="index.php">Home</a></li>
 				<li><a href="profile.php">Profile</a></li>
 				<li><a href="setting.php">Settings</a></li>
